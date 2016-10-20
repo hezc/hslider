86ec142b5060b42f040e9a4724bf1ea69ac60192
hezc <hezc.cn@qq.com>
2016-10-20 16:53:50 +0800

    调整缩进

--- src/js/hslider.js
+++ src/js/hslider.js
@@ -5,8 +5,8 @@
  */
 ;(function($) {
     if(!$) {
-		return console.warn("请加载jquery");
-	};
+        return console.warn("请加载jquery");
+    }
     var Hslider = function(ele,opts){
         var slider = this;
         slider.name = "hslider";
@@ -15,12 +15,12 @@
             index : 0,
             firstRandom: true,
             activeClass:"active",
-            autoplay: true,
+            autoPlay: true,
             delay: 8000,
             speed: 750,
             easing: 'swing', //"linear" 或 "swing"
             perView: 1,
-            
+
             // 动画选择
             // "combine3Img": 3张图片组合动画 ，电子网首页的轮播图, 默认值
             // "horizontal": 水平滑动
@@ -32,16 +32,16 @@
             nav:{
                 wrapClass: "."+slider.name + "-nav",
                 itemClass: "."+slider.name + "-n"
-                },
+            },
             pager:{
                 wrapClass : "."+ slider.name + "-page",
                 prevClass : "."+"prev",
                 nextClass : "."+"next"
-                }
+            }
         };
-        
+
         slider.options = $.extend({}, slider.defaults, opts);
-        
+
         var index = slider.options.index,
             timer = null,
             activeClass = slider.options.activeClass,
@@ -70,68 +70,67 @@
                 });
             }
         };
-        
+
         //下一个
         slider.next = function() {
-			index++;
+            index++;
             var num =  total / perView;
             if(index > num - 1){
                 index = 0;
             }
             slider.showimg(index);
             return slider;
-		};
-        
+        };
+
         //上一个
         slider.prev = function() {
-			index--;
+            index--;
             var num =  total / perView;
             if(index < 0){
                 index = num - 1;
             }
             slider.showimg(index);
             return slider;
-		};
-        
+        };
+
         //开始循环
         slider.start = function() {
-			timer = setTimeout(function() {
+            timer = setTimeout(function() {
                 slider.next();
-			}, slider.options.delay);
+            }, slider.options.delay);
+            return slider;
+        };
 
-			return slider;
-		};
-        
-        if(slider.options.autoplay){
+        if(slider.options.autoPlay){
             slider.start();
         }
-        
+
         //停止循环
-		slider.stop = function() {
-			clearTimeout(timer);
-			return slider;
-		};
-        
+        slider.stop = function() {
+            clearTimeout(timer);
+            return slider;
+        };
+
         //slider导航
         slider.setSlideNav = function (index){
             $navItems.eq(index).addClass(activeClass).siblings().removeClass(activeClass);
         };
-        
+
         slider.calculateSlides = function() {
-			// 设置容器宽度 或 高度
+            // 设置容器宽度 或 高度
             var prop = 'width';
             if(animation === 'vertical') {
                 prop = 'height';
             }
             var num =  total / perView ;
-            
+
             $contentWrap.css(prop, (num * 100) + '%');
             $contentItems.css(prop, (100 / total) + '%');
-		};
+        };
         if(animation == "horizontal" || "vertical") {
             slider.calculateSlides();
         };
-        
+
         //动画
         slider.animate = {
             combine3Img: function(){
@@ -156,7 +155,7 @@
                 $contentWrap.stop(true,true).animate({left: -(100 * index) + '%' }, slider.options.speed / 2 ,slider.options.easing);
             },
             vertical: function(){
-                
+
                 $contentItems.removeClass(activeClass);
                 var currentPerView = index * perView;
                 for(var i = 0; i< perView; i++){
@@ -166,28 +165,28 @@
                 $contentWrap.stop(true,true).animate({top: -(100 * index) + '%' }, slider.options.speed / 2 ,slider.options.easing);
             }
         };
-        
+
         //显示图片
         slider.showimg = function(index){
-            if(slider.options.autoplay) {
-				slider.stop().start();
-			}
+            if(slider.options.autoPlay) {
+                slider.stop().start();
+            }
             slider.setSlideNav(index);
             slider.imgLazy($contentWrap,index);
             slider.animate[animation]();
         };
-        
+
         //初始化随机第一项
         slider.firstRandom = function(){
             $contentWrap.prepend($contentItems.eq(indexRandom));
             $contentItems = slider.ele.find(slider.options.contentItem);
             slider.showimg(index);
         };
-        
+
         if(slider.options.firstRandom){
             slider.firstRandom();
         };
-        
+
         //图片导航
         $navItems.on("click",function(){
             index = $(this).index();
@@ -197,32 +196,32 @@
             }
             slider.showimg(index);
         });
-        
+
         //下一张图按钮
         slider.ele.find(slider.options.pager.nextClass).on("click",function(){
             slider.next();
         });
-        
+
         //上一张图按钮
         slider.ele.find(slider.options.pager.prevClass).on("click",function(){
             slider.prev();
         });
-        
+
         //鼠标悬停
-		slider.ele.hover(function(){
-             slider.stop();
-            },function(){
-             slider.start();
+        slider.ele.hover(function(){
+            slider.stop();
+        },function(){
+            slider.start();
         });
-        
-        
-        
+
+
+
     };
-    
+
     //定义 jquery.hslider 插件
     $.fn.hslider = function(options) {
         return this.each(function() {
-			var $this = $(this);
+            var $this = $(this);
             return new Hslider($this, options);
         });
     };
